d693aba902515d6f00a2c058904ce33da6684084
MartinaDR1 <derosemartina933@gmail.com>
2023-04-11 15:42:51 +0200

    generate random email

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,26 @@
+/*Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
+
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        list_mail:[]
       }
+    },
+    mounted(){
+        for(i=1; i<=10;i++){
+            axios
+            .get(" https://flynn.boolean.careers/exercises/api/random/mail ")
+            .then(response =>{
+                console.log(response);
+                this.list_mail.push(response.data.response)
+                
+            })
+        }
     }
   }).mount('#app')
